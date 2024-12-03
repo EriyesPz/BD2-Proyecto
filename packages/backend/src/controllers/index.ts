823f2e7032a8e6cb7d115b41129bf7727b0d5a7c
Erick Reyes <erickreyes.gxs@gmail.com>
2024-12-03 15:00:50 -0600

    feat: Exportando controladores

---
+++ packages/backend/src/controllers/index.ts
@@ -0,0 +1,63 @@
+export {
+  ctlInsertarConsultaMedica,
+  ctlObtenerConsultasMedicas,
+} from "./consultasMedicasController";
+export {
+  ctlGenerarFacturaHospitalizacion,
+  ctlInsertarFactura,
+  ctlInsertarPago,
+  ctlObtenerFacturas,
+} from "./facturaController";
+export { ctlInsertarUsuario, ctlUsuarios } from "./controllerUsuarios";
+export {
+  ctlInsertarDireccion,
+  ctlObtenerDirecciones,
+} from "./direccionesController";
+export {
+  ctlInsertarEmpleado,
+  ctlObtenerEmpleados,
+} from "./empleadosController";
+export {
+  ctlInsertarEspecialidad,
+  ctlObtenerEspecialidades,
+} from "./especialidadesController";
+export { ctlInsertarExamen, ctlObtenerExamenes } from "./examenesController";
+export {
+  ctlInsertarTipoHabitacion,
+  ctlObtenerTiposHabitacion,
+} from "./habitacionController";
+export {
+  ctlInsertarHabitacion,
+  ctlObtenerHabitaciones,
+} from "./habitacionesController";
+export {
+  ctlDarAltaHospitalizacion,
+  ctlObtenerHospitalizaciones,
+  ctlRegistrarHospitalizacion,
+} from "./hospitalizacionesController";
+export {
+  ctlInsertarMedicamento,
+  ctlObtenerMedicamentos,
+} from "./medicamentosController";
+export { ctlInsertarMedico, ctlObtenerMedicos } from "./medicosController";
+export {
+  ctlInsertarPaciente,
+  ctlObtenerPacientes,
+} from "./pacientesController";
+export {
+  ctlInsertarPacienteSeguro,
+  ctlObtenerPacientesSeguros,
+} from "./pacientesSegurosController";
+export {
+  ctlInsertarProveedor,
+  ctlObtenerProveedores,
+} from "./proveedoresController";
+export { ctlInsertarPuesto, ctlObtenerPuestos } from "./puestosController";
+export {
+  ctlInsertarResultadoLaboratorio,
+  ctlObtenerResultadosLaboratorio,
+} from "./resultadoLaboratorioController";
+export {
+  ctlInsertarSeguroMedico,
+  ctlObtenerSegurosMedicos,
+} from "./segurosController";
--- packages/backend/src/routes.ts
+++ packages/backend/src/routes.ts
@@ -1,10 +1,50 @@
 import { Router } from "express";
-import { ctlUsuarios } from "./controllers/controllerUsuarios";
 import { ctlAuth } from "./controllers/controllerAuth";
+import {
+  ctlDarAltaHospitalizacion,
+  ctlGenerarFacturaHospitalizacion,
+  ctlInsertarConsultaMedica,
+  ctlInsertarDireccion,
+  ctlInsertarEmpleado,
+  ctlInsertarEspecialidad,
+  ctlInsertarExamen,
+  ctlInsertarFactura,
+  ctlInsertarHabitacion,
+  ctlInsertarMedicamento,
+  ctlInsertarMedico,
+  ctlInsertarPaciente,
+  ctlInsertarPacienteSeguro,
+  ctlInsertarPago,
+  ctlInsertarProveedor,
+  ctlInsertarPuesto,
+  ctlInsertarResultadoLaboratorio,
+  ctlInsertarSeguroMedico,
+  ctlInsertarTipoHabitacion,
+  ctlInsertarUsuario,
+  ctlObtenerConsultasMedicas,
+  ctlObtenerDirecciones,
+  ctlObtenerEmpleados,
+  ctlObtenerEspecialidades,
+  ctlObtenerExamenes,
+  ctlObtenerFacturas,
+  ctlObtenerHabitaciones,
+  ctlObtenerHospitalizaciones,
+  ctlObtenerMedicamentos,
+  ctlObtenerMedicos,
+  ctlObtenerPacientes,
+  ctlObtenerPacientesSeguros,
+  ctlObtenerProveedores,
+  ctlObtenerPuestos,
+  ctlObtenerResultadosLaboratorio,
+  ctlObtenerSegurosMedicos,
+  ctlObtenerTiposHabitacion,
+  ctlRegistrarHospitalizacion,
+  ctlUsuarios,
+} from "./controllers";
 
 const router = Router();
 
 router.get("/usuarios", ctlUsuarios);
-router.post("/login", ctlAuth)
+router.post("/login", ctlAuth);
 
 export { router };
